a87a4bbdf16831ad577d609f3e68e6d213afdea9
Dmytro Yeskov <88843350+Fifth-Wave@users.noreply.github.com>
2022-01-12 18:13:28 +0200

    task 2 commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWraper = document.querySelector(".gallery");
+galleryCreation();
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function galleryCreation() {
+  galleryWraper.insertAdjacentHTML(
+    "afterbegin",
+    galleryItems.map(cardCreation).join("")
+  );
+}
+
+function cardCreation({ preview, original, description }) {
+  return ` 
+    <li>
+    <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+   </li>
+    `;
+}
